39e8b85e4e4905b3e5b8c790fe1ae75ba828a3d1
mckellbarber <mckellbarber@overstock.com>
2023-07-03 11:25:38 -0600

    add script

---
+++ script.js
@@ -0,0 +1,47 @@
+// Embarking on quests is a natural part of who we are—to explore, pursue, and discover. Whether in games or in our lives, quests provide valuable lessons and chances to grow, learn new skills, and have exciting experiences.
+// They help us gather what we need for our journey and guide us towards our ultimate purpose. To succeed, we must be ready to take on tough challenges and work hard to gain experience. We must stay focused on our main goal.
+
+// Quests are about finding what we love, meeting new people, and trying new things. So, if you happen to come across a medieval tavern, don't hesitate to ask the bartender about any local rumors.
+// While looking for monsters in real life may not be the best idea, it can lead to fascinating conversations and unexpected quest opportunities. Embrace the questing spirit, as it can reveal extraordinary paths of discovery and fulfillment.
+
+// Help:
+
+// a neighbor with their yard work
+// an elderly person with grocery shopping
+// a student with their homework
+// a charity organization with fundraising efforts
+// a local animal shelter with pet adoption events
+
+// Explore:
+
+// a new hiking trail in the area
+// a museum or art gallery in your city
+// a cultural festival or fair happening nearby
+// a local market or farmer's market
+// a historical landmark or site in your region
+
+// Learn:
+
+// a new language through online resources or classes
+// a musical instrument with the help of tutorials or a teacher
+// a coding language by enrolling in an online course
+// a new cooking technique from a recipe book or cooking show
+// a DIY project through online tutorials or workshops
+
+// Create:
+
+// a piece of artwork using different mediums (painting, sculpting, etc.)
+// a blog or personal website to share your experiences or interests
+// a handmade gift for a friend or family member
+// a home-cooked meal from scratch using fresh ingredients
+// a playlist of your favorite songs or a personalized mixtape
+
+// Support:
+
+// a local charity or nonprofit organization through volunteering or donations
+// a friend in need by lending a listening ear or offering advice
+// a small business in your community by promoting their products or services
+// a social cause or movement by participating in awareness campaigns
+// a family member or friend who is pursuing a personal goal or dream
+
+console.log('You can do this!');
